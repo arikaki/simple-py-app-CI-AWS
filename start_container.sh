74073ea7662d8a99d79c355def0bbf77a9242cff
SaiRishab <78468475+arikaki@users.noreply.github.com>
2024-10-01 21:06:55 -0400

    Updated start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sairishab/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sairishab/simple-python-flask-app:latest
